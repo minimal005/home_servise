a77644569de20fb610296a7f9bbac56537d41c41
Halyna Kravchenko <108605949+minimal005@users.noreply.github.com>
2025-02-01 22:33:47 +0200

    fix

--- config/gulp-tasks/images.js
+++ config/gulp-tasks/images.js
@@ -1,50 +1,51 @@
 import webp from "gulp-webp";
-import imagemin from "gulp-imagemin";
+import imagemin, { svgo } from "gulp-imagemin";
 
 export const images = () => {
-  return (
-    app.gulp
-      .src(app.path.src.images)
-      .pipe(
-        app.plugins.plumber(
-          app.plugins.notify.onError({
-            title: "IMAGES",
-            message: "Error: <%= error.message %>",
-          })
-        )
+  return app.gulp
+    .src(app.path.src.images)
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "IMAGES",
+          message: "Error: <%= error.message %>",
+        })
       )
-      .pipe(app.plugins.newer(app.path.build.images))
-
-      // Конвертація у WebP (якщо увімкнено)
-      .pipe(app.plugins.if(app.isWebP, webp()))
-      .pipe(app.plugins.if(app.isWebP, app.gulp.dest(app.path.build.images)))
-
-      // Оптимізація зображень (JPEG, PNG, GIF)
-      .pipe(
-        app.plugins.if(
-          app.isImgOpt,
-          imagemin({
-            progressive: true,
-            svgoPlugins: [{ removeViewBox: false }],
-            interlaced: true,
-            optimizationLevel: 3, // 0 to 7
-          })
-        )
+    )
+    .pipe(app.plugins.newer(app.path.build.images))
+    .pipe(app.plugins.if(app.isWebP, webp()))
+    .pipe(app.plugins.if(app.isWebP, app.gulp.dest(app.path.build.images)))
+    .pipe(app.plugins.if(app.isWebP, app.gulp.src(app.path.src.images)))
+    .pipe(app.plugins.if(app.isWebP, app.plugins.newer(app.path.build.images)))
+    .pipe(
+      app.plugins.if(
+        app.isImgOpt,
+        imagemin([
+          imagemin.mozjpeg({ quality: 75, progressive: true }),
+          imagemin.optipng({ optimizationLevel: 5 }),
+          svgo({
+            plugins: [
+              { removeViewBox: false },
+              // інші плагіни за потреби
+            ],
+          }),
+        ])
       )
-      .pipe(app.gulp.dest(app.path.build.images))
-
-      // Обробка SVG (додаємо оптимізацію)
-      .pipe(app.gulp.src(app.path.src.svg))
-      .pipe(
-        app.plugins.if(
-          app.isImgOpt,
-          imagemin([
-            imagemin.svgo({
-              plugins: [{ removeViewBox: false }],
-            }),
-          ])
-        )
+    )
+    .pipe(app.gulp.dest(app.path.build.images))
+    .pipe(app.gulp.src(app.path.src.svg))
+    .pipe(
+      app.plugins.if(
+        app.isImgOpt,
+        imagemin([
+          svgo({
+            plugins: [
+              { removeViewBox: false },
+              // інші плагіни за потреби
+            ],
+          }),
+        ])
       )
-      .pipe(app.gulp.dest(app.path.build.images))
-  );
+    )
+    .pipe(app.gulp.dest(app.path.build.images));
 };
